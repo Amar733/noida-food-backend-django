98945d9e4e55341d80500ceff08f47c34fa7f2db
Amar733 <amarkumar00702@gmail.com>
2026-08-08 23:29:15 +0530

    add

--- app1/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Certificate
-
-class CertificateForm(forms.ModelForm):
-    class Meta:
-        model = Certificate
-        fields = ['name', 'email', 'course']
